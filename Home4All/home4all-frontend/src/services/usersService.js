3e4568b855714e579966c22608db662654a5e7a2
afonscosta <afonso.costa.15@gmail.com>
2019-05-11 13:08:39 +0100

    vue register user

---
+++ Home4All/home4all-frontend/src/services/usersService.js
@@ -0,0 +1,13 @@
+import home4all from "@/services/home4all";
+
+export default {
+  fetchUsers() {
+    return home4all.get(`users/`).then(response => response.data);
+  },
+  postUser(payload) {
+    return home4all.post(`users/`, payload).then(response => response.data);
+  },
+  deleteUser(userId) {
+    return home4all.delete(`users/${userId}`).then(response => response.data);
+  }
+};
