977fd38fca469d98cc4fbc23a92854e714d14893
Marco Silva <marant_silva@hotmail.com>
2019-06-26 10:31:16 +0100

    starting complaints pages in vue js

---
+++ Home4All/home4all-frontend/src/views/Complaints.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container>
+    <div class="title">
+      <b-row align-h="center">
+        <b-col cols="12" sm="11" md="9">
+          <h1>Complaints</h1>
+          <b-card bg-variant="light" title="Card Title">
+            <b-card-text>
+              With s
+            </b-card-text>
+            <b-button variant="primary">Go somewhere</b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'complaints',
+  data: () => {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+.title {
+  text-align: left
+}
+
+</style>
