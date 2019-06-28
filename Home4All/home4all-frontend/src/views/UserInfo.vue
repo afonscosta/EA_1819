f8b32f923a14e5f70f890786910cc3ea7d069789
afonscosta <afonso.costa.15@gmail.com>
2019-06-28 08:48:39 +0100

    delete user

--- Home4All/home4all-frontend/src/views/UserInfo.vue
+++ Home4All/home4all-frontend/src/views/UserInfo.vue
@@ -71,7 +71,7 @@
               </b-form-group>
 
               <b-button type="submit" variant="primary">Guardar</b-button>
-              <b-button @click="deleteUserAccount" class="ml-2" variant="danger">Eliminar conta</b-button>
+              <b-button @click="deleteUserAccount; $router.push('/')" class="ml-2" variant="danger">Eliminar conta</b-button>
 
             </b-form>
           </b-col>
